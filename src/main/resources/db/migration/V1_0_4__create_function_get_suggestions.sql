cca3ba5fef5b4dc378fcd9f32c91b21312c6b613
Kevin Christian <kdotchrist30@gmail.com>
2025-01-18 07:03:30 +0700

    feat: handle get suggestions and calculate score without lat and long

---
+++ src/main/resources/db/migration/V1_0_4__create_function_get_suggestions.sql
@@ -0,0 +1,39 @@
+drop function get_suggestions;
+
+CREATE OR REPLACE FUNCTION get_suggestions(
+	search_query TEXT,
+	start_lat DOUBLE PRECISION,
+	start_long DOUBLE PRECISION
+)
+RETURNS TABLE (
+    secure_id VARCHAR(36),
+    name TEXT,
+    lat DOUBLE PRECISION,
+    long DOUBLE PRECISION,
+    score DOUBLE PRECISION
+) AS
+$$
+DECLARE
+	keywords TEXT[];
+BEGIN
+	RETURN QUERY
+    SELECT
+    	mvs.secure_id,
+    	mvs."name",
+    	mvs.lat,
+    	mvs.long,
+    	(
+    		SELECT *
+    		FROM calculate_score(
+    			search_query,
+    			(SELECT * FROM string_to_array(mvs.query, ',')),
+    			start_lat,
+    			start_long,
+    			mvs.lat,
+    			mvs.long
+    		)
+    	) AS score
+    FROM materialized_view_suggestions mvs
+   	ORDER BY score DESC;
+END;
+$$ LANGUAGE plpgsql;
